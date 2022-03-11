8851f516ac2eba963692bafa9eb57d66b89eb20c
nozeg <100212429+nozeg@users.noreply.github.com>
2022-03-11 14:48:11 +0100

    Modifs page d'accueil

---
+++ src/components/LienProjet.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let titre;
+  export let lien;
+  export let image;
+  export let colorbox;
+
+  const imageLien = `url(${image})`;
+  // si image vaut "toto", imageLien vaut "url(toto)"
+</script>
+
+<a class="projetINV" href={lien} style="--bgImage:{imageLien};--Colorprojet:{colorbox}">
+  <span>{titre}</span>
+</a>
+
+<style>
+  .projetINV {
+    width: 333px;
+    height: 333px;
+    background-image: var(--bgImage);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    transition-property: background-color;
+    transition-duration: 150ms;
+    display: flex;
+    justify-content: center;
+    padding: 16px;
+    text-decoration: none;
+  }
+
+  .projetINV span {
+    color: white;
+    opacity: 0;
+    align-self: flex-end;
+  }
+
+  .projetINV:hover {
+    background-color: var(--Colorprojet);
+    background-image: none;
+  }
+
+  .projetINV:hover span {
+    opacity: 1;
+    font-weight: 800;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,28 @@
+<script>
+  import LienProjet from '../components/LienProjet.svelte';
+</script>
+
 <div class="listeprojets">
-  <a class="projetINV" href="/projet">
-    <span> INV - Villes invisibles </span>
-  </a>
+  <LienProjet
+    titre="INV - Villes invisibles"
+    lien="/projet"
+    image="/inv-img/inv-1-01-01.jpg"
+    colorbox="#c96f44"
+  />
+  <LienProjet
+    titre="Poubelle de table"
+    lien="/projet"
+    image="/inv-img/Cremaillere.gif"
+    colorbox="#37553e"
+  />
+  <LienProjet titre="INV - Villes invisibles" lien="/projet" />
 </div>
 
 <style>
   .listeprojets {
-    height: 300vh;
+    display: flex;
+    flex-wrap: wrap;
     flex-grow: 1;
     margin: 16px 0px 16px 0px;
-  }
-
-  .projetINV {
-    width: 333px;
-    height: 333px;
-    background-image: url('/inv-img/inv-1-01-01.jpg');
-    background-size: contain;
-    transition-property: background-color;
-    transition-duration: 150ms;
-    display: flex;
-    justify-content: center;
-    padding: 16px;
-    text-decoration: none;
-  }
-
-  .projetINV span {
-    color: white;
-    opacity: 0;
-    align-self: flex-end;
-  }
-
-  .projetINV:hover {
-    background-color: #c96f44;
-    background-image: none;
-  }
-
-  .projetINV:hover span {
-    opacity: 1;
-    font-weight: 800;
   }
 </style>
