e1b3feb31240d183ed6d033d6109f2477773b15d
nozeg <100212429+nozeg@users.noreply.github.com>
2022-04-15 19:02:48 +0000

    Deploying to gh-pages from @ nozeg/metis_studio@692b4b838e1880b3b99dbb5e8b992e19ec020f6d 🚀

---
+++ _app/chunks/testbis-293b2263.js
@@ -0,0 +1,1 @@
+const t={images:[{src:"/Vivants.gif",alt:"vivants"}],description:"DescriptionBis",titre:"Projet bis",color:"#dedede",lien:"test-bis",thumbnail:"/About-01.jpg"};export{t as default};
---
+++ _app/pages/boutique.svelte-3f9187d0.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s as o}from"../chunks/vendor-289dee53.js";class n extends t{constructor(s){super();e(this,s,null,null,o,{})}}export{n as default};
