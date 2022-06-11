b2c970d39ab7bb691785a8f63360c8626c092d13
nozeg <100212429+nozeg@users.noreply.github.com>
2022-06-11 17:21:15 +0200

    Modifs VAL, SAT et ISL

--- src/routes/isl.svelte
+++ src/routes/isl.svelte
@@ -1,79 +1,20 @@
-<div class="flex">
-  <div class="content">
-    <img src="/isl/03.jpg" alt="Metis studio" />
-    <img src="/isl/01.jpg" alt="Metis studio" />
-    <img src="/isl/02.jpg" alt="Metis studio" />
-  </div>
-  <div class="description">
-    <div class="description-fixed">
-      <img class="WIP" src="/about/Travail-EC.svg" alt="Metis studio" />
+<script>
+  import Content from '$components/projets/Content.svelte';
+  import Description from '$components/projets/Description.svelte';
+  import Tags from '$components/projets/Tags.svelte';
+  import Layout from '$components/projets/Layout.svelte';
+  import Image from '$components/projets/Image.svelte';
+</script>
 
-      <div class="titre">ISL - L'Islande, une terre de contraste</div>
-      <div class="description-txt">Bonjour</div>
+<Layout>
+  <Description titre="ISL - L'Islande, une terre de contraste" wip>
+    A venir !
 
-      <div class="description-txt" />
-
-      <div class="tags">illustration ; islande ; carnet de voyage ; en cours</div>
-    </div>
-  </div>
-</div>
-
-<style>
-  .flex {
-    display: flex;
-  }
-
-  .content {
-    flex-grow: 1;
-    justify-content: center;
-  }
-
-  .description {
-    width: 400px;
-    flex-shrink: 0;
-  }
-
-  .description-fixed {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 400px;
-    padding: 100px 30px 16px 30px;
-    text-align: justify;
-    color: #0c795c;
-    font-size: 14px;
-    background-color: white;
-  }
-
-  .titre {
-    font-weight: 800;
-    font-size: 18px;
-    margin-bottom: 30px;
-  }
-
-  .description-txt {
-    margin-bottom: 10px;
-  }
-
-  .tags {
-    font-size: 12px;
-    font-weight: 700;
-    color: #19a880;
-    margin-top: 50px;
-    margin-bottom: 500px;
-  }
-
-  .WIP {
-    margin-top: -10%;
-    margin-bottom: 10px;
-    margin-left: 80%;
-    width: 20%;
-  }
-
-  img {
-    margin: 0;
-    padding: 0;
-    max-width: 100%;
-    max-height: 100vh;
-  }
-</style>
+    <Tags>illustration ; islande ; carnet de voyage ; en cours</Tags>
+  </Description>
+  <Content>
+    <Image src="/isl/03.jpg" alt="Metis studio" />
+    <Image src="/isl/01.jpg" alt="Metis studio" />
+    <Image src="/isl/02.jpg" alt="Metis studio" />
+  </Content>
+</Layout>
--- src/routes/sat.svelte
+++ src/routes/sat.svelte
@@ -1,103 +1,37 @@
-<div class="flex">
-  <div class="content">
-    <img src="/sat-img/01.jpg" alt="Metis studio" />
-    <img src="/sat-img/02.jpg" alt="Metis studio" />
-    <img src="/sat-img/03.jpg" alt="Metis studio" />
-    <img src="/sat-img/04.jpg" alt="Metis studio" />
-    <img src="/sat-img/05.jpg" alt="Metis studio" />
-    <img src="/sat-img/06.jpg" alt="Metis studio" />
-  </div>
-  <div class="description">
-    <div class="description-fixed">
-      <img class="WIP" src="/about/Travail-EC.svg" alt="Metis studio" />
+<script>
+  import Content from '$components/projets/Content.svelte';
+  import Description from '$components/projets/Description.svelte';
+  import Tags from '$components/projets/Tags.svelte';
+  import Layout from '$components/projets/Layout.svelte';
+  import Image from '$components/projets/Image.svelte';
+</script>
 
-      <div class="titre">SAT - Ecole chantier</div>
-      <div class="description-txt">
-        Tout est mouvement. Tout se réinvente. Notre école va changer ! Aborder son futur, c’est
-        avant tout s’intéresser, participer et contribuer à sa construction. Transformons notre
-        école, comme nous nous transformons. En développant notre conscience de ce lieu que nous
-        habitons la plupart du temps. Allons-y ! Devenons les créateurs et mettons en perspective
-        nos propres envies et obligations. Civisme social, amical, généreux et collaboratif, avec
-        nos amis et nos parents. C’est comme cela que la vie en société se fait, c’est pourtant
-        simple, vous comprenez ?
-      </div>
-
-      <div class="description-txt">
-        Un projet à linitative d'Irene Gil López, Yoana Milian Lucas et Cédric Muller.
-      </div>
-
-      <div class="description-txt">
-        Suivez les actualités d'Ecole Chantier ici : <a
-          href="https://www.ecolechantier.ch"
-          target="_blank">www.ecolechantier.ch</a
-        >
-      </div>
-
-      <div class="description-txt">Photographies de Paola Corsini.</div>
-
-      <div class="tags">
-        outil de médiation ; satigny ; enfants ; école ; processus participatif ; en cours
-      </div>
+<Layout>
+  <Description titre="SAT - Ecole chantier" wip>
+    Tout est mouvement. Tout se réinvente. Notre école va changer ! Aborder son futur, c’est avant
+    tout s’intéresser, participer et contribuer à sa construction. Transformons notre école, comme
+    nous nous transformons. En développant notre conscience de ce lieu que nous habitons la plupart
+    du temps. Allons-y ! Devenons les créateurs et mettons en perspective nos propres envies et
+    obligations. Civisme social, amical, généreux et collaboratif, avec nos amis et nos parents.
+    C’est comme cela que la vie en société se fait, c’est pourtant simple, vous comprenez ?
+    <div>Un projet à linitative d'Irene Gil López, Yoana Milian Lucas et Cédric Muller.</div>
+    <div>
+      Suivez les actualités d'Ecole Chantier ici : <a
+        href="https://www.ecolechantier.ch"
+        target="_blank">www.ecolechantier.ch</a
+      >
     </div>
-  </div>
-</div>
 
-<style>
-  .flex {
-    display: flex;
-  }
+    Photographies de Paola Corsini.
 
-  .content {
-    flex-grow: 1;
-    justify-content: center;
-  }
-
-  .description {
-    width: 400px;
-    flex-shrink: 0;
-  }
-
-  .description-fixed {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 400px;
-    padding: 100px 30px 16px 30px;
-    text-align: justify;
-    color: #0c795c;
-    font-size: 14px;
-    background-color: white;
-  }
-
-  .titre {
-    font-weight: 800;
-    font-size: 18px;
-    margin-bottom: 30px;
-  }
-
-  .description-txt {
-    margin-bottom: 10px;
-  }
-
-  .tags {
-    font-size: 12px;
-    font-weight: 700;
-    color: #19a880;
-    margin-top: 50px;
-    margin-bottom: 500px;
-  }
-
-  .WIP {
-    margin-top: -10%;
-    margin-bottom: 10px;
-    margin-left: 80%;
-    width: 20%;
-  }
-
-  img {
-    margin: 0;
-    padding: 0;
-    max-width: 100%;
-    max-height: 100vh;
-  }
-</style>
+    <Tags>outil de médiation ; satigny ; enfants ; école ; processus participatif ; en cours</Tags>
+  </Description>
+  <Content>
+    <Image src="/sat-img/01.jpg" alt="Metis studio" />
+    <Image src="/sat-img/02.jpg" alt="Metis studio" />
+    <Image src="/sat-img/03.jpg" alt="Metis studio" />
+    <Image src="/sat-img/04.jpg" alt="Metis studio" />
+    <Image src="/sat-img/05.jpg" alt="Metis studio" />
+    <Image src="/sat-img/06.jpg" alt="Metis studio" />
+  </Content>
+</Layout>
