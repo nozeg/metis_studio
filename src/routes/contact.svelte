3f6227374b5a251b29d9ec598dd1e7ce9a2ec653
nozeg <100212429+nozeg@users.noreply.github.com>
2022-06-30 14:16:22 +0200

    modifs page contact img mise en page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import caesar from '$lib/caesar';
   import TitreVert from '$components/TitreVert.svelte';
+  import Tapis2 from '$components/Tapis2.svelte';
 
   const hashedEmail = 'ijpm)h`odn)nop_dj;bh\\dg)^jh';
   let email = hashedEmail;
@@ -28,29 +29,39 @@
     </div>
   </div>
   <div class="content">
-    <img src="/contact/Dessin-contact.jpg" alt="Metis studio" />
+    <Tapis2 />
   </div>
 </div>
 
 <style>
   .flex {
+    width: 100%;
     display: flex;
-    flex-wrap: wrap;
-    margin-bottom: 40px;
+    flex-grow: 1;
+
+    flex-direction: column;
+  }
+
+  @media (min-width: 768px) {
+    .flex {
+      justify-content: space-evenly;
+      flex-direction: row;
+    }
   }
 
   .content {
     flex-grow: 1;
     flex-basis: 300px;
     justify-content: center;
-  }
-
-  .description {
-    flex-basis: 400px;
+    margin-top: 10px;
   }
 
   .description-fixed {
-    padding: 100px 30px 16px 30px;
+    max-width: 400px;
+    margin-top: 50px;
+    margin-left: 10px;
+    margin-bottom: 2%;
+    padding: 10px;
     text-align: justify;
     color: #0c795c;
     font-size: 14px;
@@ -64,17 +75,12 @@
     font-family: bakso;
     font-weight: 700;
     font-size: 14px;
+    padding: 10px;
     padding-top: 50px;
     padding-bottom: 30px;
   }
   .description-txt {
+    padding: 10px;
     margin-bottom: 10px;
-  }
-
-  img {
-    margin: 0;
-    padding: 0;
-    max-width: 100%;
-    max-height: 100vh;
   }
 </style>
