e570860737298c16d1a1f7968f427ec15371c066
nozeg <100212429+nozeg@users.noreply.github.com>
2022-06-30 14:56:39 +0200

    modifs page contact

--- src/routes/about3.svelte
+++ src/routes/about3.svelte
@@ -1,19 +1,85 @@
-<div id="un">
-  <div id="deux">jkdf jgkgj ljmlk fgkmgd fgd gdfdfg vgbfbc</div>
+<script>
+  import Titresvertslongs from '$components/Titresvertslongs.svelte';
+</script>
+
+<div class="flex">
+  <div class="content">
+    <div class="title-container">
+      <Titresvertslongs>Métis Studio, qu'est-ce que c'est ?</Titresvertslongs>
+    </div>
+
+    <div class="texte">
+      Procédé d'impression à l'aide d'un écran constitué par un cadre sur lequel est tendu un tissu
+      à mailles, permettant l'impression sur de multiples surfaces.
+    </div>
+  </div>
+  <img class="photo" src="/outils-img/ser1.jpg" alt="Metis studio" />
+  <img class="photo" src="/outils-img/ser2.jpg" alt="Metis studio" />
+  <img class="photo" src="/outils-img/ser3.jpg" alt="Metis studio" />
+</div>
+
+<div class="flex">
+  <div class="content">
+    <div class="title-container">
+      <Titresvertslongs>Métis Studio, quels services ?</Titresvertslongs>
+    </div>
+    <div class="texte">
+      Procédé d'impression dérivé de l'offset, utilisant un système de stencil et permettant des
+      tirages en couleur économiques.
+    </div>
+  </div>
+  <img class="photo" src="/outils-img/riso.jpg" alt="Metis studio" />
+</div>
+
+<div class="flex">
+  <div class="content">
+    <div class="title-container">
+      <Titresvertslongs>Pourquoi Métis studio ?</Titresvertslongs>
+    </div>
+    <div class="texte">
+      Procédé d'impression dérivé de l'offset, utilisant un système de stencil et permettant des
+      tirages en couleur économiques.
+    </div>
+  </div>
+  <img class="photo" src="/outils-img/riso.jpg" alt="Metis studio" />
+</div>
+
+<div class="flex">
+  <div class="content">
+    <div class="title-container">
+      <Titresvertslongs>Qui est derrière Métis Studio ?</Titresvertslongs>
+    </div>
+    <div class="texte">
+      Procédé d'impression dérivé de l'offset, utilisant un système de stencil et permettant des
+      tirages en couleur économiques.
+    </div>
+  </div>
+  <img class="photo" src="/outils-img/riso.jpg" alt="Metis studio" />
 </div>
 
 <style>
-  .un {
-    background-image: url(http://img6.imageshack.us/img6/6806/o2bj.jpg);
-  }
-
-  .deux {
-    display: none;
-    background-color: white;
-    opacity: 0.5;
-  }
-
-  .un:hover #deux {
+  .photo {
+    max-width: 100%;
+    height: auto;
     display: block;
+    margin-top: 5px;
+  }
+
+  .flex {
+    margin-top: 10%;
+    margin-bottom: 10%;
+  }
+
+  .content {
+    max-width: 400px;
+    margin-top: 2%;
+    margin-left: 10px;
+    margin-bottom: 2%;
+    font-size: 14px;
+    color: #007d5e;
+  }
+
+  .title-container {
+    margin-bottom: 32px;
   }
 </style>
