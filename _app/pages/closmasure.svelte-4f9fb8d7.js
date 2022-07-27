226cb13c3e896140d4d23c6d7dfcc56c0eaad1d7
nozeg <100212429+nozeg@users.noreply.github.com>
2022-07-27 11:45:02 +0000

    Deploying to gh-pages from @ nozeg/metis_studio@d2016e81bfede05c838ee4a09deb2cc3e49afce9 🚀

---
+++ _app/pages/closmasure.svelte-4f9fb8d7.js
@@ -0,0 +1,21 @@
+import{S as h,i as b,s as q,w as i,x as l,y as u,q as c,o as p,B as f,k as x,m as j,g as m,d as $,t as _,h as v,I as L}from"../chunks/vendor-981532c1.js";import{L as w,D as C,C as D,T as A,I as P}from"../chunks/Image-1695b8b4.js";function B(o){let e;return{c(){e=_(`outil de m\xE9diation ; centre-ville ; cartographie ; architecture ; d\xE9pliant ; affiches ; en
+      cours`)},l(n){e=v(n,`outil de m\xE9diation ; centre-ville ; cartographie ; architecture ; d\xE9pliant ; affiches ; en
+      cours`)},m(n,t){m(n,e,t)},d(n){n&&$(e)}}}function I(o){let e,n,t;return n=new A({props:{$$slots:{default:[B]},$$scope:{ctx:o}}}),{c(){e=_(`Le centre-ville de Bellegarde-sur-Valserine regorge de tr\xE9sors. Pourtant la plupart du temps, on
+    passe devant sans les voir, sans leur pr\xEAter attention. Le projet propose de mettre en lumi\xE8re
+    les diff\xE9rents b\xE2timents Art D\xE9co qui font, en partie, la singularit\xE9 du centre-ville. Cet
+    inventaire non exhaustif (et en cours\xA0!) recense les b\xE2timents connus comme l\u2019H\xF4tel de ville, la
+    Poste ou encore le cin\xE9ma mais pas que. Des b\xE2timents plus discrets, plus confidentiels, de
+    l\u2019ordre de l\u2019usage quotidien sont ainsi r\xE9v\xE9l\xE9s aux curieux. L\u2019objectif du projet est de cr\xE9er
+    une cartographie de ces b\xE2timents en faisant figurer sur une carte un \xE9l\xE9ment de d\xE9cor
+    identitaire et propre \xE0 chaque b\xE2timent. Une fois sur place il ne reste plus qu\u2019\xE0 jouer \xE0
+    chercher et trouver cet \xE9l\xE9ment\xA0!
+    `),i(n.$$.fragment)},l(s){e=v(s,`Le centre-ville de Bellegarde-sur-Valserine regorge de tr\xE9sors. Pourtant la plupart du temps, on
+    passe devant sans les voir, sans leur pr\xEAter attention. Le projet propose de mettre en lumi\xE8re
+    les diff\xE9rents b\xE2timents Art D\xE9co qui font, en partie, la singularit\xE9 du centre-ville. Cet
+    inventaire non exhaustif (et en cours\xA0!) recense les b\xE2timents connus comme l\u2019H\xF4tel de ville, la
+    Poste ou encore le cin\xE9ma mais pas que. Des b\xE2timents plus discrets, plus confidentiels, de
+    l\u2019ordre de l\u2019usage quotidien sont ainsi r\xE9v\xE9l\xE9s aux curieux. L\u2019objectif du projet est de cr\xE9er
+    une cartographie de ces b\xE2timents en faisant figurer sur une carte un \xE9l\xE9ment de d\xE9cor
+    identitaire et propre \xE0 chaque b\xE2timent. Une fois sur place il ne reste plus qu\u2019\xE0 jouer \xE0
+    chercher et trouver cet \xE9l\xE9ment\xA0!
+    `),l(n.$$.fragment,s)},m(s,r){m(s,e,r),u(n,s,r),t=!0},p(s,r){const a={};r&1&&(a.$$scope={dirty:r,ctx:s}),n.$set(a)},i(s){t||(c(n.$$.fragment,s),t=!0)},o(s){p(n.$$.fragment,s),t=!1},d(s){s&&$(e),f(n,s)}}}function S(o){let e,n;return e=new P({props:{src:"/clos/Arbres-03.jpg",alt:"Metis studio"}}),{c(){i(e.$$.fragment)},l(t){l(e.$$.fragment,t)},m(t,s){u(e,t,s),n=!0},p:L,i(t){n||(c(e.$$.fragment,t),n=!0)},o(t){p(e.$$.fragment,t),n=!1},d(t){f(e,t)}}}function H(o){let e,n,t,s;return e=new C({props:{titre:"MAS - A la d\xE9couverte des clos-masure",wip:!0,$$slots:{default:[I]},$$scope:{ctx:o}}}),t=new D({props:{$$slots:{default:[S]},$$scope:{ctx:o}}}),{c(){i(e.$$.fragment),n=x(),i(t.$$.fragment)},l(r){l(e.$$.fragment,r),n=j(r),l(t.$$.fragment,r)},m(r,a){u(e,r,a),m(r,n,a),u(t,r,a),s=!0},p(r,a){const d={};a&1&&(d.$$scope={dirty:a,ctx:r}),e.$set(d);const g={};a&1&&(g.$$scope={dirty:a,ctx:r}),t.$set(g)},i(r){s||(c(e.$$.fragment,r),c(t.$$.fragment,r),s=!0)},o(r){p(e.$$.fragment,r),p(t.$$.fragment,r),s=!1},d(r){f(e,r),r&&$(n),f(t,r)}}}function M(o){let e,n;return e=new w({props:{$$slots:{default:[H]},$$scope:{ctx:o}}}),{c(){i(e.$$.fragment)},l(t){l(e.$$.fragment,t)},m(t,s){u(e,t,s),n=!0},p(t,[s]){const r={};s&1&&(r.$$scope={dirty:s,ctx:t}),e.$set(r)},i(t){n||(c(e.$$.fragment,t),n=!0)},o(t){p(e.$$.fragment,t),n=!1},d(t){f(e,t)}}}class V extends h{constructor(e){super();b(this,e,null,M,q,{})}}export{V as default};
