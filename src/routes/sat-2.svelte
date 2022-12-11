8b28d8b38b6f24c2146cc9023186c7cd8245d31b
nozeg <100212429+nozeg@users.noreply.github.com>
2022-12-11 17:38:03 +0100

    Ajout Satimini

---
+++ src/routes/sat-2.svelte
@@ -0,0 +1,74 @@
+<script>
+  import Content from '$components/projets/Content.svelte';
+  import Description from '$components/projets/Description.svelte';
+  import Tags from '$components/projets/Tags.svelte';
+  import Layout from '$components/projets/Layout.svelte';
+  import Image from '$components/projets/Image.svelte';
+  import Map from '$components/Map.svelte';
+</script>
+
+<Layout>
+  <Description titre="SATIMINI - Histoires courtes de Satigny - Ecole chantier" wip>
+    <p>
+      Peut-être l’avez-vous vu : fin septembre, les élèves de l’École Mairie ont peint une fresque
+      géante sur le chemin de leur école transitoire. Cette fresque fait écho à la belle vue et à
+      l’ambiance de Satigny, propice à la contemplation.
+    </p>
+
+    <p>
+      Vous pouvez toujours aller la découvrir entre le croisement de la route du Mandement et la
+      Mairie de Satigny. Ce n’est pas tout ! Ils ont également enregistré des histoires touchantes
+      et émouvantes, sous la forme de poèmes que vous pourrez directement écouter pendant votre
+      balade grâce à des codes QR. Au total, ce sont sept classes d’élèves qui ont participé à cette
+      magnifique exposition en plein air, dirigés par les enseignants en arts plastiques Laure
+      Fontana et Raphael Bernardez. Cette aventure n’aurait pas été possible sans la bienveillante
+      aide de Mme Sabrine Paracchini, directrice des établissements du Mandement, la collaboration
+      active de l’équipe de la cohésion sociale ainsi que la disponibilité permanente des équipes
+      liées aux infrastructures de la commune.Pour fêter cette occasion, nous avons organisé le
+      samedi 15 octobre à la salle Montfleury un apéritif «fresque mais en mini», où les convives
+      pouvaient prendre un bout de trottoir (une focaccia onctueuse à la couleur noire) afin de
+      réaliser et manger quelques fresques gourmandes.
+    </p>
+    <p>
+      De nombreuses personnes ont fait le déplacement et l’ambiance était colorée et conviviale.
+      Nous remercions toutes et tous les participants pour leur présence dans la première
+      intervention du processus participatif ! Le prochain rendez-vous École Chantier est agendé en
+      début Avril 2023 et sera l’occasion de réaliser une sculpture en forme de mosaïque avec des
+      morceaux de l’ancienne école. Cette œuvre collective sera ensuite intégrée à la nouvelle école
+      et sera l’empreinte du temps qui s’écoule au rythme scolaire. Il y aura également un événement
+      - exposition où vous pourrez toutes et tous participer et créer votre sculpture gastronomique.
+      Nous nous réjouissons de vous retrouver et vous souhaitons de magnifiques fêtes de fin d’année
+      !
+    </p>
+    <p>Un projet à linitative d'Irene Gil López, Yoana Milian Lucas et Cédric Muller.</p>
+    <p>
+      Suivez les actualités d'Ecole Chantier ici : <a
+        href="https://www.ecolechantier.ch"
+        target="_blank">www.ecolechantier.ch</a
+      >
+    </p>
+
+    Photographies de Paola Corsini.
+
+    <Tags>outil de médiation ; satigny ; enfants ; école ; processus participatif ; en cours</Tags>
+    <Map
+      mapCenter={[46.21803875669701, 6.030364858651818]}
+      mapMarker={[46.21803875669701, 6.030364858651818]}
+      mapZoom={17}
+    />
+  </Description>
+  <Content>
+    <Image src="/sat-2/01-2.jpg" alt="Metis studio" />
+    <Image src="/sat-2/01-1.jpg" alt="Metis studio" />
+    <Image src="/sat-2/01-3.jpg" alt="Metis studio" />
+    <Image src="/sat-2/02-1.jpg" alt="Metis studio" />
+    <Image src="/sat-2/02-2.jpg" alt="Metis studio" />
+    <Image src="/sat-2/03-1.jpg" alt="Metis studio" />
+    <Image src="/sat-2/02-4.jpg" alt="Metis studio" />
+    <Image src="/sat-2/03-1.jpg" alt="Metis studio" />
+    <Image src="/sat-2/03-2.jpg" alt="Metis studio" />
+    <Image src="/sat-2/03-3.jpg" alt="Metis studio" />
+    <Image src="/sat-2/03-4.jpg" alt="Metis studio" />
+    <Image src="/sat-2/03-5.jpg" alt="Metis studio" />
+  </Content>
+</Layout>
